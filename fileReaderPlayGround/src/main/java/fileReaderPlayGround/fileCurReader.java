cb9e38e4b585fbcfcfdd43404929588d937f00ba
Tony Chi <mp4056@gmail.com>
2021-04-14 09:33:25 +0800

    fileReader
    程式行數結果與實際行數落差已解決
    原因: 使用了FileReader讀取ZIP檔
    
    新增問題: 嘗試清除map中重複key並相加value值時，經過判斷依然出現key重複問題且value並未相加

--- fileReaderPlayGround/src/main/java/fileReaderPlayGround/fileCurReader.java
+++ fileReaderPlayGround/src/main/java/fileReaderPlayGround/fileCurReader.java
@@ -1,6 +1,7 @@
 package fileReaderPlayGround;
 
 import java.io.BufferedInputStream;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
@@ -11,16 +12,21 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 import java.util.zip.ZipInputStream;
 
-import com.opencsv.CSVReader;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class fileCurReader {
 
 	public static void main(String[] args) {
-
 		File dir = new File("C:\\Users\\Tony Chi\\Desktop\\Programming\\Amazon Billing\\amazon-billing\\CUR");
 		showDirectoryFiles(dir);
 	}
@@ -32,25 +38,57 @@ public class fileCurReader {
 				showDirectoryFiles(file);
 			} else {
 				calculateRows(file);
-				System.out.println("files:" + file.getName());
-				System.out.println("belongs to:" + file.getParent());
-				
 			}
+
 		}
 	}
 
-	public static int calculateRows(File file) {
-		int count = 0;
+	public static void calculateRows(File file) {
+	
 		try {
-			BufferedReader in = new BufferedReader(new FileReader(file));
-			while((in.readLine())!=null) {
-				count++;
-			}
-			System.out.println("count="+count);
-			in.close();
-		}catch(Exception e) {
+			ZipInputStream zipInputStream = new ZipInputStream(new FileInputStream(file));
+			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(zipInputStream));
+			while ((zipInputStream.getNextEntry() != null)) {				
+				String line = "";
+				int usageAccountId = 0;
+				while ((line = bufferedReader.readLine()) != null) {
+					String[] resultSet = line.split(",");
+					Map<String, Integer> map = new HashMap<>();
+					usageAccountId = map.containsKey(resultSet[8]) ? map.get(resultSet[8]) : 0;
+					map.remove("lineItem/UsageAccountId");
+					map.put(resultSet[8], usageAccountId + 1);
+					valueAdder(map);
+					ObjectMapper objectMapper = new ObjectMapper();
+					String finalResult = objectMapper.writeValueAsString(map);
+					
+				}		
+			}	
+			zipInputStream.close();
+			bufferedReader.close();
+		} catch (IOException e) {
 			e.printStackTrace();
-		}
-		return count -1;
+		}	
 	}
+	
+	public static Map<String,Integer> valueAdder(Map<String,Integer> map){
+		
+		Set<Map.Entry<String, Integer>>entrySet = map.entrySet();
+		Iterator<Map.Entry<String, Integer>> it = entrySet.iterator();
+		Map <String,Integer> resultSet = new HashMap<>();	
+		while(it.hasNext()) {	
+			Map.Entry<String, Integer> me = it.next();	
+			String key = me.getKey();
+			int value = me.getValue();
+			if(resultSet.containsKey(key)) {
+				//resultSet現在是空值，但Key是有東西的
+				resultSet.put(key, resultSet.get(key)+value);
+			}else {
+				resultSet.put(key, value);				
+			}
+			System.out.println("resultSet="+resultSet);
+		}
+		return map;
+	}
+	
+	
 }
